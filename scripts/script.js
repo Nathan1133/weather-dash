bc5ed70a4a03e05804802b2e8e5ec3ebbdb20143
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-04-22 22:46:00 -0400

    buttons, api and local storage

--- scripts/script.js
+++ scripts/script.js
@@ -31,3 +31,44 @@ var searchedCities = [];
         };
         
         createList();
+
+        //OnClick event for search button
+        searchButton.on("click", function(event) {
+            event.preventDefault();
+            var cityName = input[0].value; 
+            
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=metric&uvi?&appid=95d304ea9130c998e905d74bc71292d7";
+            
+              // Performing an AJAX request with the queryURL
+              $.ajax({
+                url: queryURL,
+                method: "GET",
+                crossDomain: true,
+                dataType: 'jsonp'
+              })
+                // After data comes back from the request
+                .then(function(response) {
+                var cityWeather = JSON.stringify(response);    
+        
+                var iconCode = response.weather[0].icon;
+                var iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
+        
+                // Append results
+                $(".city-name").html(response.name + " Weather Details:");
+                $(".date").html(timeDateToday);
+                $(".conditions").html("Conditions: " + response.weather[0].main + "<img id='weather-icon' src='" + iconURL + "' alt='Weather icon'>");
+                $(".wind").text("Wind Speed: " + response.wind.speed);
+                $(".humidity").text("Humidity: " + response.main.humidity);
+                $(".temp-C").html("Temperature (°C): " + response.main.temp);
+                });
+        
+                // Get item from local storate
+              var citiesTemp = JSON.parse(localStorage.getItem("searchedCities"));
+              citiesTemp.push(cityName);
+                // Set item to local storage
+              localStorage.setItem("searchedCities", JSON.stringify(citiesTemp));
+              // Call the functions
+              generateList();
+              createList();
+        
+          });
