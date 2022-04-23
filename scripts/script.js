da3a814ec1e03fedf76537282594ba0465a59f84
Nathan1133 <94014154+Nathan1133@users.noreply.github.com>
2022-04-23 09:46:53 -0400

    Finished js

--- scripts/script.js
+++ scripts/script.js
@@ -72,3 +72,39 @@ var searchedCities = [];
               createList();
         
           });
+
+            // OnClick event for city buttons
+  $(".featured-cities").on("click", function(event) {
+    event.preventDefault();
+    var cityText = $(event.target).attr("id");  
+    
+   
+    var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q="+cityText+"&units=metric&uvi?&appid=95d304ea9130c998e905d74bc71292d7";
+      
+      // Performing an AJAX request with the queryURL2
+      $.ajax({
+        url: queryURL2,
+        method: "GET",
+        crossDomain: true,
+        dataType: 'jsonp'
+      })
+
+        // After data comes back from the request
+        .then(function(response) {
+        var cityWeather = JSON.stringify(response);
+            
+            var iconCode = response.weather[0].icon;
+            var iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
+
+        // Append results
+        $(".city-name").html(response.name + " Weather Details:");
+        $(".date").html(timeDateToday);
+        $(".conditions").html("Conditions: " + response.weather[0].main + "<img id='weather-icon' src='" + iconURL + "' alt='Weather icon'>");
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".temp-C").html("Temperature (°C): " + response.main.temp);
+        });
+
+        generateList();
+
+    });
